52c8a5d8cd76dc4d9730de831c609a28524c3008
Gustavo <gstvrrds@gmail.com>
2022-11-08 15:34:04 -0300

    colocando classes das tags pelo jquery e trocando background de Mais conteudos

--- js/mais_conteudos.js
+++ js/mais_conteudos.js
@@ -3,39 +3,45 @@ var testa = document.querySelector(".testa");
 var conteudo = {
     conteudos:[
         {
-            tag:"Nature",
+            tag:"Water",
             titulo:"Text Random",
             autor:"/assets/img__autor.svg",
             nome_autor:"Gustavo",
-            data_lançamento:"nov,05 2022"
+            data_lançamento:"nov,05 2022",
+            background:"bg-[url(/assets/imagemConteudoTorneira.svg)]"
         },
         {
             tag:"Nature",
             titulo:"Text Random",
             autor:"/assets/img__autor.svg",
             nome_autor:"Gustavo",
-            data_lançamento:"nov,05 2022"
+            data_lançamento:"nov,05 2022",
+            background:"bg-[url(/assets/imagemConteudoTerra.svg)]",
+            
         },
         {
             tag:"Nature",
             titulo:"Text Random",
             autor:"/assets/img__autor.svg",
             nome_autor:"Gustavo",
-            data_lançamento:"nov,05 2022"
+            data_lançamento:"nov,05 2022",
+            background:"bg-[url(/assets/imagemConteudo.svg)]"
         },
         {
-            tag:"Nature",
+            tag:"Water",
             titulo:"Text Random",
             autor:"/assets/img__autor.svg",
             nome_autor:"Gustavo",
-            data_lançamento:"nov,05 2022"
+            data_lançamento:"nov,05 2022",
+            background:"bg-[url(/assets/imagemConteudoOceano.svg)]"
+            
         }
     ]
 }
 window.onload = function criaMaisConteudos(){
 
 for (let i = 0; i < 4; i++) {
-    var conteudinho= '<p class="Bg__frase frase__conteudo dentro__conteudo">'+ conteudo.conteudos[i].tag +"</p>";
+    var conteudinho= '<p class="Bg__frase frase__conteudo dentro__conteudo '+conteudo.conteudos[i].tag+'">'+ conteudo.conteudos[i].tag +"</p>";
 
     conteudinho += '<h2 class="titulo__conteudo dentro__conteudo">'+ conteudo.conteudos[i].titulo +"</h2>";
 
@@ -43,10 +49,11 @@ for (let i = 0; i < 4; i++) {
 
     conteudinho += '<p class="nome_autor_conteudo">'+ conteudo.conteudos[i].nome_autor +"</p>";
     conteudinho+= '<div class="flex relogio">'+'<img src="/assets/img__relogio.svg" alt="" class="relogio__conteudo dentro__conteudo relogio__teste">'+'<p class="data__hora__conteudo ">'+conteudo.conteudos[i].data_lançamento+'</div>';
-    conteudoCompleto = '<a href="#" class="imagem__conteudo">' + conteudinho +'</a>';
+    conteudoCompleto = '<a href="#" class="imagem__conteudo '+ conteudo.conteudos[i].background +'">' + conteudinho + '</a>';
     console.log(conteudinho)
     $(".testa").append(conteudoCompleto)
     
+    //<p class="Bg__frase frase__conteudo dentro__conteudo">Nature</p><h2 class="titulo__conteudo dentro__conteudo">Text Random</h2><img src = "/assets/img__autor.svg" alt class="autor__conteudo dentro__conteudo"></img><p class="nome_autor_conteudo">Gustavo</p><div class="flex relogio"><img src="/assets/img__relogio.svg" alt="" class="relogio__conteudo dentro__conteudo relogio__teste"><p class="data__hora__conteudo ">nov,05 2022</div>
 }
     
 }
