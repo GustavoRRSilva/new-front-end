29aa7575b5fc55814e98afeea88e1d648280b520
Gustavo <gstvrrds@gmail.com>
2022-11-10 01:16:29 -0300

    teste 1 noticias

--- js/mais_conteudos.js
+++ js/mais_conteudos.js
@@ -45,10 +45,10 @@ for (let i = 0; i < 4; i++) {
 
     conteudinho += '<h2 class="titulo__conteudo dentro__conteudo">'+ conteudo.conteudos[i].titulo +"</h2>";
 
-    conteudinho +='<img src = "'+conteudo.conteudos[i].autor +'" alt class="autor__conteudo dentro__conteudo">'+'</img>';
+    conteudinho +='<div class="flex autorCompleto justify-center items-center gap-x-1"><img src = "'+conteudo.conteudos[i].autor +'" alt class="autor__conteudo dentro__conteudo  ml-0 ">'+'</img>';
 
-    conteudinho += '<p class="nome_autor_conteudo">'+ conteudo.conteudos[i].nome_autor +"</p>";
-    conteudinho+= '<div class="flex relogio">'+'<img src="/assets/img__relogio.svg" alt="" class="relogio__conteudo dentro__conteudo relogio__teste">'+'<p class="data__hora__conteudo ">'+conteudo.conteudos[i].data_lançamento+'</div>';
+    conteudinho += '<p class="nome_autor_conteudo">'+ conteudo.conteudos[i].nome_autor +"</p></div>";
+    conteudinho+= '<div class="flex relogio ">'+'<img src="/assets/img__relogio.svg" alt="" class="relogio__conteudo dentro__conteudo relogio__teste">'+'<p class="data__hora__conteudo ">'+conteudo.conteudos[i].data_lançamento+'</div>';
     conteudoCompleto = '<a href="#" class="imagem__conteudo '+ conteudo.conteudos[i].background +'">' + conteudinho + '</a>';
     console.log(conteudinho)
     $(".testa").append(conteudoCompleto)
